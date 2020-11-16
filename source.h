9facb71f9fd971abba63a3fda6c31b9a046a5bff
Merideth Safflower <msafflow@ga-c1.msk.21-school.ru>
2020-11-17 00:17:44 +0300

    gnl added

--- source.h
+++ source.h
@@ -6,7 +6,7 @@
 /*   By: msafflow <msafflow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 23:26:25 by msafflow          #+#    #+#             */
-/*   Updated: 2020/11/17 00:17:17 by msafflow         ###   ########.fr       */
+/*   Updated: 2020/11/17 00:17:36 by msafflow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ struct		s_source
 	long	curpos;
 }			t_source;
 
-char	next_char(struct source_s *src);
-void	unget_char(struct source_s *src);
-char	peek_char(struct source_s *src);
-void	skip_white_spaces(struct source_s *src);
+char		next_char(struct source_s *src);
+void		unget_char(struct source_s *src);
+char		peek_char(struct source_s *src);
+void		skip_white_spaces(struct source_s *src);
 
 #endif
